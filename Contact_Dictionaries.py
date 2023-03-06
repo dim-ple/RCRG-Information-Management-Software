2bf131a5f110730df7da1da80f205f4416c53828
Harrison Goehring <harrison.goehring@gmail.com>
2023-03-06 15:11:15 -0500

    Import Contact Dictionaries module
    Used for testing our Info Management program. Internal Data is entered into Data Dictionaries so main program can run without the use of a Database. This code is temporary.

---
+++ Contact_Dictionaries.py
@@ -0,0 +1,30 @@
+#Realtor & Lender Dictionary Call List
+
+rcrg = {'Christine': ['christinemottleyrva@gmail.com;', 'Christine Mottley', 'Christine', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Christine Mottley', '(804)586-0765'],
+        'Greg': ['gregsellsva@gmail.com;', 'Greg Lewis', 'Greg', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Greg Lewis', '(804)307-6999'], 
+        'Eleni': ['eleni@findhomerva.com;', 'Eleni Filippone', 'Eleni', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Eleni Filippone', '(804)433-9113'],
+        'Brett': ['brettmlynes@gmail.com;', 'Brett Lynes', 'Brett', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Brett Lynes', '(804)715-2155'],
+        'Tunde': ['tundehasthekey@gmail.com;', 'Tunde Lewis', 'Tunde', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Tunde Lewis', '(804)307-7008'], 
+        'Melanie': ['melanies1274@yahoo.com;', 'Melanie Sutherland', 'Melanie', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Melanie Sutherland', '(804)595-1576'],
+        'Kathy': ['kathyhole1@gmail.com;', 'Kathy Hole', 'Kathy', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Kathy Hole', '(804)221-7570'], 
+        'Jessica': ['jpi.raices@gmail.com;', 'Jessica Pi', 'Jessica', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Jessica Pi', '(703)865-1106'],
+        'Rick': ['rick@rickcoxrealty.com;', 'Rick Cox', 'Rick', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Rick Cox', '(804)920-1738'], 
+        'Harrison': ['harrison@rickcoxrealty.com;', 'Harrison Goehring', 'Harrison', '', '(804)447-2834'],
+        'Amy': ['amy@rickcoxrealty.com;', 'Amy Foldes', 'Amy', '', '(804)447-2834'], 
+        'Matt': ['mbarlowrvahomes@gmail.com;', 'Matt Barlow', 'Matt', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Matt Barlow', '(804)380-0757'],
+        'Jackson': ['jacksonkimrva@gmail.com;', 'Jackson Kim', 'Jackson', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Jackson Kim', '(804)651-0101'],
+        'Other':['NEED AGENT NAME', '', '', '', '']
+        }
+
+lender = {'Annie-Mac': ['nmatheny@annie-mac.com; bschultz@annie-mac.com;', 'Nathan & Brent', 'Nathan Matheny'], 'Alcova': ['ecraig@alcova.com; lsmith@alcova.com;', 'Eric & Lisa', 'Eric Craig'], 
+          'Presidential': ['belynda.wilson@presidential.com', 'Belynda', 'Belynda Wilson'], 'Other':['NEED LENDER NAME', ' ', ' ']
+           }
+
+attorney = {'Lane & Hamner': ['contracts@laneandhamner.com', 'Susan & Jenn', 'Lane & Hamner', 'Susan Dyke'], 'Cowan Gates': ['nmccallum@cowangates.com', 'Natalie', 'Cowan Gates', 'Natalie McCallum'],
+            'Atlantic Coast': ['susan@acsettlement.com', 'Susan', 'Atlantic Coast Settlement Services', 'Susan Cooper'], 'Hairfield Morton': ['dreynolds@hmalaw.com', 'Debby', 'Hairfield-Morton PLC', 'Debby Reynolds'],
+            'Kane&Jeffries Henrico': ['KSL@kanejeffries.com; gg@kanejeffries.com', 'Kris', 'Kane & Jeffries'], 
+            'Appomattox Title': ['crystal@apptitle.com', 'Crystal', 'Appomattox Title', 'Crystal Davis'], 'Kane&Jeffries Midlo': ['tracy@kanejeffries.com', 'Tracy', 'Kane & Jeffries', 'Tracy Edwards'], 
+            'Shaheen Law': ['mvazquez@shaheenlaw.com', 'Maritza', 'Shaheen Law', 'Maritza Vasquez'], 'Other':['NEED ATTORNEY CONTACT NAME', '', '', '']
+            }
+
+
