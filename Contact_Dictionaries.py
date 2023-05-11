195018e470ad7c5d5565683b9e261ad21624fbba
Harrison Goehring <98855700+dim-ple@users.noreply.github.com>
2023-05-11 11:42:19 -0400

    Updated RCRG Contact Dictionary
    Updated RCRG Contact Dictionary. 
    -Jessica Pi
    -Jackson Kim
    +Morgan Porter

--- Contact_Dictionaries.py
+++ Contact_Dictionaries.py
@@ -6,18 +6,17 @@ rcrg = {'Christine': ['christinemottleyrva@gmail.com;', 'Christine Mottley', 'Ch
         'Brett': ['brettmlynes@gmail.com;', 'Brett Lynes', 'Brett', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Brett Lynes', '(804)715-2155'],
         'Tunde': ['tundehasthekey@gmail.com;', 'Tunde Lewis', 'Tunde', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Tunde Lewis', '(804)307-7008'], 
         'Melanie': ['melanies1274@yahoo.com;', 'Melanie Sutherland', 'Melanie', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Melanie Sutherland', '(804)595-1576'],
-        'Kathy': ['kathyhole1@gmail.com;', 'Kathy Hole', 'Kathy', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Kathy Hole', '(804)221-7570'], 
-        'Jessica': ['jpi.raices@gmail.com;', 'Jessica Pi', 'Jessica', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Jessica Pi', '(703)865-1106'],
+        'Kathy': ['kathyhole1@gmail.com;', 'Kathy Hole', 'Kathy', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Kathy Hole', '(804)221-7570'],
         'Rick': ['rick@rickcoxrealty.com;', 'Rick Cox', 'Rick', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Rick Cox', '(804)920-1738'], 
         'Harrison': ['harrison@rickcoxrealty.com;', 'Harrison Goehring', 'Harrison', '', '(804)447-2834'],
         'Amy': ['amy@rickcoxrealty.com;', 'Amy Foldes', 'Amy', '', '(804)447-2834'], 
         'Matt': ['mbarlowrvahomes@gmail.com;', 'Matt Barlow', 'Matt', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Matt Barlow', '(804)380-0757'],
-        'Jackson': ['jacksonkimrva@gmail.com;', 'Jackson Kim', 'Jackson', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Jackson Kim', '(804)651-0101'],
+        'Morgan': ['morgan@byrdpm.com', 'Morgan Porter', 'Morgan', 'C:\\Users\\rcrgr\\RickCoxRealty Dropbox\\RickCoxRealty Team Folder\\Open Files\\Listing_Buyers Morgan Porter', '(804)304-9020'],
         'Other':['NEED AGENT NAME', '', '', '', '']
         }
 
-lender = {'Annie-Mac': ['nmatheny@annie-mac.com; bschultz@annie-mac.com;', 'Nathan & Brent', 'Nathan Matheny'], 'Alcova': ['ecraig@alcova.com; lsmith@alcova.com;', 'Eric & Lisa', 'Eric Craig'], 
-          'Presidential': ['belynda.wilson@presidential.com', 'Belynda', 'Belynda Wilson'], 'Other':['NEED LENDER NAME', ' ', ' ']
+lender = {'C & F Mortgage': ['nmatheny@cfmortgagecorp.com', 'Nathan', 'Nathan Matheny', '804-909-3022'], 'Alcova': ['ecraig@alcova.com; lsmith@alcova.com;', 'Eric & Lisa', 'Eric Craig', '804-380-0757'], 
+          'Presidential': ['belynda.wilson@presidential.com', 'Belynda', 'Belynda Wilson', '804-712-0045'], 'Other':['NEED LENDER NAME', ' ', ' ', ' ']
            }
 
 attorney = {'Lane & Hamner': ['contracts@laneandhamner.com', 'Susan & Jenn', 'Lane & Hamner', 'Susan Dyke'], 'Cowan Gates': ['nmccallum@cowangates.com', 'Natalie', 'Cowan Gates', 'Natalie McCallum'],
