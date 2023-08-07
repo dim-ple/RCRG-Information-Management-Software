4fe4dad73fcb4b341f69d7c60a09873ea26d06d6
Harrison Goehring <harrison.goehring@gmail.com>
2023-08-07 15:25:55 -0400

    Update combox test.py

--- combox test.py
+++ combox test.py
@@ -12,31 +12,30 @@ select = tk.StringVar()
 
 def update(data):
     new_list = []
-    
-    my_combo.delete()
 
     for item in data: 
         new_list.append(item)
     
-    my_combo['values'] = new_list
+    my_combo["values"] = new_list
 
 def fillout(event):
-    my_combo.delete()
-    my_combo.insert(my_combo.get(ACTIVE))
+
+    my_combo.delete(0, END)
+    my_combo.insert(0, my_combo.get())
 
 def check(event):
     # Grab what was typed
-    typed = my_combo.get()
+    typed = select
 
     # When nothing is typed, the entry list is populated with all toppings from the toppings list
     if typed == '':
-        data = my_combo['values']
+        data = my_combo["values"]
     else:
         data = []
         for item in data:
             if typed.lower() in item.lower():
                 data.append(item)
-    
+
     # update our listbox with selected items
     update(data)
 
@@ -48,9 +47,8 @@ my_combo.pack(pady=40)
 
 my_combo["values"] = ["Test1", "Nest2", "Lest3", "Rest3", "Hello", "Gamer"]
 
-
-
 update(my_combo["values"])
+
 
 my_combo.bind("<<ComboboxSelected>>", fillout)
 
