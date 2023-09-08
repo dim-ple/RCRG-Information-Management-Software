32f9ff8aaa19503fa058b821f12b2e4d382f1bc1
Harrison Goehring <harrison.goehring@gmail.com>
2023-09-08 15:19:25 -0400

    Abstracted SQLPopList Function away from main

--- RCRG_Email_Program.py
+++ RCRG_Email_Program.py
@@ -29,43 +29,7 @@ import sqlite3
 
 # Empty lists to be utilized by the tkinter UI. Populated by the brute force for-loops below
 # **Eventually will be replaced with SQL query population**
-def SQLPopList(table_name):
-    
-    new_list = []
-    key_database = []
-    dict = {}
-
-    try:
-        conn = sqlite3.connect('rcrgbroker.db')
-        c = conn.cursor()
-        print("Successfully Connected to Database!")
-
-        c.execute(f'SELECT * FROM {table_name}')
-
-        for row in c:
-            new_list.append(row)
-
-    except:
-        print("There was an error connecting to the RCRG Database.")
-
-    finally:
-        c.close()
-        conn.close()
-        print("Connection to Database Closed.")
-
-    
-    if table_name == 'rcrg':
-        for i, row in enumerate(new_list):
-            key_database.append(new_list[i][1] + " " + new_list[i][2])
-    else:
-        for i, row in enumerate(new_list):
-            key_database.append(new_list[i][1] + " (" + new_list[i][2] + ")")
-
-
-    for j, entry in enumerate(new_list):
-            dict[key_database[j]] = entry
-
-    return key_database, dict
+from SQLPopList import SQLPopList
 
 
 
--- SQLPopList.py
+++ SQLPopList.py
@@ -1,25 +1,39 @@
 import sqlite3
 
-def SQLPopList(table_name, list_name):
+def SQLPopList(table_name):
     
     new_list = []
+    key_database = []
+    dict = {}
 
     try:
-        conn = sqlite3.connect('rcrgbrokerage.db')
+        conn = sqlite3.connect('rcrgbroker.db')
         c = conn.cursor()
         print("Successfully Connected to Database!")
 
         c.execute(f'SELECT * FROM {table_name}')
 
         for row in c:
-                new_list.append(row)
+            new_list.append(row)
 
     except:
-        print("Hello Error!")
+        print("There was an error connecting to the RCRG Database.")
 
     finally:
         c.close()
         conn.close()
-        print("Connection to Database Closed!")
+        print("Connection to Database Closed.")
 
-    list_name = new_list
+    
+    if table_name == 'rcrg':
+        for i, row in enumerate(new_list):
+            key_database.append(new_list[i][1] + " " + new_list[i][2])
+    else:
+        for i, row in enumerate(new_list):
+            key_database.append(new_list[i][1] + " (" + new_list[i][2] + ")")
+
+
+    for j, entry in enumerate(new_list):
+            dict[key_database[j]] = entry
+
+    return key_database, dict
